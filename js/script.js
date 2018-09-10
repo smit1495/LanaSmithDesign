9318d3064d1c3786da8b01a27166b3d7184d049b
Lana <32076493+smit1495@users.noreply.github.com>
2018-09-10 19:44:08 -0400

    Now desktop friendly

--- js/script.js
+++ js/script.js
@@ -5,12 +5,17 @@ var modal01 = document.getElementById('myModal');
 
 // Get the button that opens the modal
 var btn01 = document.getElementById("myBtn");
+var btnn01 = document.getElementById("myBtnn");
 
 // Get the <span> element that closes the modal
 var span01 = document.getElementsByClassName("close01")[0];
 
 // When the user clicks the button, open the modal 
-btn01.onclick = function() {
+btn01.onclick = function () {
+    modal01.style.display = "block";
+}
+
+btnn01.onclick = function () {
     modal01.style.display = "block";
 }
 
@@ -29,12 +34,17 @@ var modal02 = document.getElementById('myModal02');
 
 // Get the button that opens the modal
 var btn02 = document.getElementById("myBtn02");
+var btnn02 = document.getElementById("myBtnn02");
 
 // Get the <span> element that closes the modal
 var span02 = document.getElementsByClassName("close02")[0];
 
 // When the user clicks the button, open the modal 
 btn02.onclick = function() {
+    modal02.style.display = "block";
+}
+
+btnn02.onclick = function() {
     modal02.style.display = "block";
 }
 
@@ -54,12 +64,17 @@ var modal03 = document.getElementById('myModal03');
 
 // Get the button that opens the modal
 var btn03 = document.getElementById("myBtn03");
+var btnn03 = document.getElementById("myBtnn03");
 
 // Get the <span> element that closes the modal
 var span03 = document.getElementsByClassName("close03")[0];
 
 // When the user clicks the button, open the modal 
 btn03.onclick = function() {
+    modal03.style.display = "block";
+}
+
+btnn03.onclick = function() {
     modal03.style.display = "block";
 }
 
@@ -79,12 +94,17 @@ var modal04 = document.getElementById('myModal04');
 
 // Get the button that opens the modal
 var btn04 = document.getElementById("myBtn04");
+var btnn04 = document.getElementById("myBtnn04");
 
 // Get the <span> element that closes the modal
 var span04 = document.getElementsByClassName("close04")[0];
 
 // When the user clicks the button, open the modal 
 btn04.onclick = function() {
+    modal04.style.display = "block";
+}
+
+btnn04.onclick = function() {
     modal04.style.display = "block";
 }
 
@@ -103,12 +123,17 @@ var modal05 = document.getElementById('myModal05');
 
 // Get the button that opens the modal
 var btn05 = document.getElementById("myBtn05");
+var btnn05 = document.getElementById("myBtnn05");
 
 // Get the <span> element that closes the modal
 var span05 = document.getElementsByClassName("close05")[0];
 
 // When the user clicks the button, open the modal 
 btn05.onclick = function() {
+    modal05.style.display = "block";
+}
+
+btnn05.onclick = function() {
     modal05.style.display = "block";
 }
 
@@ -127,12 +152,17 @@ var modal06 = document.getElementById('myModal06');
 
 // Get the button that opens the modal
 var btn06 = document.getElementById("myBtn06");
+var btnn06 = document.getElementById("myBtnn06");
 
 // Get the <span> element that closes the modal
 var span06 = document.getElementsByClassName("close06")[0];
 
 // When the user clicks the button, open the modal 
 btn06.onclick = function() {
+    modal06.style.display = "block";
+}
+
+btnn06.onclick = function() {
     modal06.style.display = "block";
 }
 
@@ -152,12 +182,17 @@ var modal07 = document.getElementById('myModal07');
 
 // Get the button that opens the modal
 var btn07 = document.getElementById("myBtn07");
+var btnn07 = document.getElementById("myBtnn07");
 
 // Get the <span> element that closes the modal
 var span07 = document.getElementsByClassName("close07")[0];
 
 // When the user clicks the button, open the modal 
 btn07.onclick = function() {
+    modal07.style.display = "block";
+}
+
+btnn07.onclick = function() {
     modal07.style.display = "block";
 }
 
@@ -175,12 +210,17 @@ var modal08 = document.getElementById('myModal08');
 
 // Get the button that opens the modal
 var btn08 = document.getElementById("myBtn08");
+var btnn08 = document.getElementById("myBtnn08");
 
 // Get the <span> element that closes the modal
 var span08 = document.getElementsByClassName("close08")[0];
 
 // When the user clicks the button, open the modal 
 btn08.onclick = function() {
+    modal08.style.display = "block";
+}
+
+btnn08.onclick = function() {
     modal08.style.display = "block";
 }
 
@@ -197,12 +237,17 @@ var modal09 = document.getElementById('myModal09');
 
 // Get the button that opens the modal
 var btn09 = document.getElementById("myBtn09");
+var btnn09 = document.getElementById("myBtnn09");
 
 // Get the <span> element that closes the modal
 var span09 = document.getElementsByClassName("close09")[0];
 
 // When the user clicks the button, open the modal 
 btn09.onclick = function() {
+    modal09.style.display = "block";
+}
+
+btnn09.onclick = function() {
     modal09.style.display = "block";
 }
 
@@ -219,12 +264,17 @@ var modal10 = document.getElementById('myModal10');
 
 // Get the button that opens the modal
 var btn10 = document.getElementById("myBtn10");
+var btnn10 = document.getElementById("myBtnn10");
 
 // Get the <span> element that closes the modal
 var span10 = document.getElementsByClassName("close10")[0];
 
 // When the user clicks the button, open the modal 
 btn10.onclick = function() {
+    modal10.style.display = "block";
+}
+
+btnn10.onclick = function() {
     modal10.style.display = "block";
 }
 
@@ -241,12 +291,17 @@ var modal11 = document.getElementById('myModal11');
 
 // Get the button that opens the modal
 var btn11 = document.getElementById("myBtn11");
+var btnn11 = document.getElementById("myBtnn11");
 
 // Get the <span> element that closes the modal
 var span11 = document.getElementsByClassName("close11")[0];
 
 // When the user clicks the button, open the modal 
 btn11.onclick = function() {
+    modal11.style.display = "block";
+}
+
+btnn11.onclick = function() {
     modal11.style.display = "block";
 }
 
@@ -262,12 +317,17 @@ var modal12 = document.getElementById('myModal12');
 
 // Get the button that opens the modal
 var btn12 = document.getElementById("myBtn12");
+var btnn12 = document.getElementById("myBtnn12");
 
 // Get the <span> element that closes the modal
 var span12 = document.getElementsByClassName("close12")[0];
 
 // When the user clicks the button, open the modal 
 btn12.onclick = function() {
+    modal12.style.display = "block";
+}
+
+btnn12.onclick = function() {
     modal12.style.display = "block";
 }
 
@@ -284,12 +344,17 @@ var modal13 = document.getElementById('myModal13');
 
 // Get the button that opens the modal
 var btn13 = document.getElementById("myBtn13");
+var btnn13 = document.getElementById("myBtnn13");
 
 // Get the <span> element that closes the modal
 var span13 = document.getElementsByClassName("close13")[0];
 
 // When the user clicks the button, open the modal 
 btn13.onclick = function() {
+    modal13.style.display = "block";
+}
+
+btnn13.onclick = function() {
     modal13.style.display = "block";
 }
 
@@ -305,12 +370,17 @@ var modal14 = document.getElementById('myModal14');
 
 // Get the button that opens the modal
 var btn14 = document.getElementById("myBtn14");
+var btnn14 = document.getElementById("myBtnn14");
 
 // Get the <span> element that closes the modal
 var span14 = document.getElementsByClassName("close14")[0];
 
 // When the user clicks the button, open the modal 
 btn14.onclick = function() {
+    modal14.style.display = "block";
+}
+
+btnn14.onclick = function() {
     modal14.style.display = "block";
 }
 
@@ -326,12 +396,17 @@ var modal15 = document.getElementById('myModal15');
 
 // Get the button that opens the modal
 var btn15 = document.getElementById("myBtn15");
+var btnn15 = document.getElementById("myBtnn15");
 
 // Get the <span> element that closes the modal
 var span15 = document.getElementsByClassName("close15")[0];
 
 // When the user clicks the button, open the modal 
 btn15.onclick = function() {
+    modal15.style.display = "block";
+}
+
+btnn15.onclick = function() {
     modal15.style.display = "block";
 }
 
